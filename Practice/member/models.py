cc928dc16d99f3b860495b35392381e7530991c5
Bum-Jung, Yong <dydqjawnd@gmail.com>
2021-02-13 13:15:14 +0900

    Edit member/register app

--- Practice/member/models.py
+++ Practice/member/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class BoardMember(models.Model):
     username    = models.CharField(max_length=100, verbose_name='유저Id')
-    email       = models.EmailField(max_length=100, verbose_name='유저메일')
     password    = models.CharField(max_length=100, verbose_name='유저PW')
+    email       = models.EmailField(max_length=100, verbose_name='유저메일')
     create_at   = models.DateTimeField(auto_now_add=True, verbose_name='가입날짜')
     update_at   = models.DateTimeField(auto_now=True, verbose_name='마지막수정일')
 
@@ -11,8 +11,7 @@ class BoardMember(models.Model):
     def __str__(self):
         return self.username
 
-    # 데이터베이스에 저장되는 테이블명
     class Meta:
-        db_table            = 'boardmembers'
-        verbose_name        = '게시판멤버'
-        verbose_name_plural = '게시판멤버'
+        db_table            = 'boardmembers' # 데이터베이스에 저장되는 테이블명
+        verbose_name        = '게시판멤버' # 해당 테이블을 조회할 때 테이블이름
+        verbose_name_plural = '게시판멤버들' # 해당 테이블을 조회할 때 테이블이름 (기본값 = "verbose_name"+s)
--- Practice/member/views.py
+++ Practice/member/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import BoardMember
 
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
     elif request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        #re_password = request.POST['re_password']
+        username    = request.POST.get('username', None)
+        password    = request.POST['password']
+        email       = request.POST.get('email', None)
 
-        member = BoardMember(
-            username=username,
-            password=password,
-            email=email,
-        )
-        member.save()
+        res_data = {}
+        if not (username and email):
+            res_data['error'] = '모든 값을 입력해주세요.'
+        else:
+            member = BoardMember(
+                username = username,
+                password = password,
+                email = email,
+            )
+            member.save() # 데이터베이스에 저장
 
-        return render(request, 'register.html') # 유지? 삭제?
+        return render(request, 'register.html', res_data) # res_data를 넘겨서 html을 request
+
+# 다음일정
+# https://velog.io/@teddybearjung/Django-%EB%A1%9C-%EA%B2%8C%EC%8B%9C%ED%8C%90-%EB%A7%8C%EB%93%A4%EA%B8%B010.-Login-%ED%99%94%EB%A9%B4-templates-%EB%A7%8C%EB%93%A4%EA%B8%B0-login-%ED%95%A8%EC%88%98-%EC%9E%91%EC%84%B1
